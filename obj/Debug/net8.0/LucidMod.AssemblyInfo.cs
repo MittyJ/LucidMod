80dbff06434529087ebc545a944d22477743f7ee
MittyJ <neddie.jerden@gmail.com>
2024-07-22 21:55:39 -0400

    0.1.1: Changed Display name and Description

--- obj/Debug/net8.0/LucidMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/LucidMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LucidMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f45add433833897ca4ed5ff318ec6e6c9d9142b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8524075f543eac0f619ab618b28baf16a30b80da")]
 [assembly: System.Reflection.AssemblyProductAttribute("LucidMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LucidMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
